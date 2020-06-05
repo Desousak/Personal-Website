1220e78a2df516e66cf0ef6d024716e5a3def343
Kevin Desousa <kevin.desousa1@ontariotechu.net>
2020-06-05 14:23:20 -0400

    Fixed scrolling issue

--- src/App.js
+++ src/App.js
@@ -10,22 +10,6 @@ function App() {
       <Header />
       <Projects />
       <Extras />
-      
-
-
-      {/* <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
     </div>
   );
 }
