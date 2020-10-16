343765d4d9217a15bd48536f910966ef4d3520e0
Kevin Desousa <kevindesousa3@gmail.com>
2020-10-15 20:57:27 -0400

    Added support for different screen sizes

--- src/App.js
+++ src/App.js
@@ -2,21 +2,12 @@
 import React from 'react';
 import NameCard from './NameCard.js';
 import Particles from './Particles.js';
-import AOS from 'aos';
 
 // Style sheets
 import './App.css';
-import 'aos/dist/aos.css';
+import './screen-sizes.css'
 
 function App() {
-  // Prep animation library
-  AOS.init({
-    duration: 1500,
-    delay: 125,
-    once: false,
-    mirror: true
-  });
-
   return (
     <div id="app">
       <div id="grid-wrapper">
